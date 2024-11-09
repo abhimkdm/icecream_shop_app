e5b5f9e8e04151ed99af5fc2db0fb483f1cca99c
Abhishek.M.Kadam <abhishek.m.kadam@netcracker.com>
2024-11-09 10:03:46 +0530

    ReactJs Login Form with Validations

--- src/components/Auth/Login/Login.tsx
+++ src/components/Auth/Login/Login.tsx
@@ -1,17 +1,78 @@
 import { Container, TextField, Typography, Button, Box } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 
 const Login: React.FC = () => {
-    return <>
-        <Container>
-            <Box sx={{ width: 500, marginLeft: 40 }}>
-            <Typography variant="h1">LogIn</Typography>
-            <TextField id="email" fullWidth margin="normal" label="Email" variant="outlined" />
-            <TextField id="passsword" fullWidth margin="normal" label="Password" variant="outlined" />
-            <Button variant="contained" fullWidth>LogIn</Button>
-            </Box>
-        </Container>
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [emailErr, setEmailErr] = useState<string>("");
+  const [passwordErr, setPasswordErr] = useState<string>("");
+
+  //Event 
+  const handleLogin = (e: React.FormEvent) =>{
+    e.preventDefault();
+    console.log("Submit");
+
+    // console.log(email);
+    // console.log(password);
+
+    //Rest error messages
+    setEmailErr("");
+    setPasswordErr("");
+
+    //Set the error messages
+    const errors = {
+        email: email === "" ? "Please enter valid email." : "",
+        password: password === "" ? "Plesae enter valid password." : ""
+    }
+
+    //Login Form validations
+    if(errors.email || errors.password)
+    {
+        setEmailErr(errors.email);
+        setPasswordErr(errors.password);
+        return;
+    }
+
+  }
+
+  return (
+    <>
+      <Container>
+        <Box sx={{ width: 500, marginLeft: 40 }}>
+        <form onSubmit={handleLogin}>
+          <Typography variant="h1">LogIn</Typography>
+          <TextField
+            id="email"
+            fullWidth
+            margin="normal"
+            label="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            variant="outlined"
+            type="text"
+            error={!!emailErr}
+            helperText={emailErr}
+          />
+          <TextField
+            id="passsword"
+            fullWidth
+            margin="normal"
+            label="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            variant="outlined"
+            type="password"
+            error={!!passwordErr}
+            helperText={passwordErr}
+          />
+          <Button type="submit" variant="contained" fullWidth>
+            LogIn
+          </Button>
+          </form>
+        </Box>
+      </Container>
     </>
-}
+  );
+};
 
 export default Login;
